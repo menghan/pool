7fd4dd147f0ee95d4b65185453845d0d51fb7eeb
Menghan <menghan412@gmail.com>
2015-07-08 15:35:45 +0800

    cut

--- pool/event.py
+++ pool/event.py
@@ -5,67 +5,6 @@ from . import util, exc
 CANCEL = util.symbol('CANCEL')
 NO_RETVAL = util.symbol('NO_RETVAL')
 
-def listen(target, identifier, fn, *args, **kw):
-    """Register a listener function for the given target.
-    
-    e.g.::
-    
-        from sqlalchemy import event
-        from sqlalchemy.schema import UniqueConstraint
-        
-        def unique_constraint_name(const, table):
-            const.name = "uq_%s_%s" % (
-                table.name,
-                list(const.columns)[0].name
-            )
-        event.listen(
-                UniqueConstraint, 
-                "after_parent_attach", 
-                unique_constraint_name)
-
-    """
-
-    for evt_cls in _registrars[identifier]:
-        tgt = evt_cls._accept_with(target)
-        if tgt is not None:
-            tgt.dispatch._listen(tgt, identifier, fn, *args, **kw)
-            return
-    raise exc.InvalidRequestError("No such event '%s' for target '%s'" %
-                                (identifier,target))
-
-def listens_for(target, identifier, *args, **kw):
-    """Decorate a function as a listener for the given target + identifier.
-    
-    e.g.::
-    
-        from sqlalchemy import event
-        from sqlalchemy.schema import UniqueConstraint
-        
-        @event.listens_for(UniqueConstraint, "after_parent_attach")
-        def unique_constraint_name(const, table):
-            const.name = "uq_%s_%s" % (
-                table.name,
-                list(const.columns)[0].name
-            )
-    """
-    def decorate(fn):
-        listen(target, identifier, fn, *args, **kw)
-        return fn
-    return decorate
-
-def remove(target, identifier, fn):
-    """Remove an event listener.
-
-    Note that some event removals, particularly for those event dispatchers
-    which create wrapper functions and secondary even listeners, may not yet
-    be supported.
-
-    """
-    for evt_cls in _registrars[identifier]:
-        for tgt in evt_cls._accept_with(target):
-            tgt.dispatch._remove(identifier, tgt, fn, *args, **kw)
-            return
-
 _registrars = util.defaultdict(list)
 
 def _is_event_name(name):
